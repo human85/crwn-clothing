4ba3ccc63f2a01375eae14e8842eab401d6a8499
陈浩 <chenhao2110@qq.com>
2023-11-17 19:07:48 +0800

    added in addToCart feature

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,13 @@
 import './product.styles.scss'
 import Button from '../button/button.component'
+import { CartContext } from '@/contexts/cart.context'
+import { useContext } from 'react'
 
 function ProductCard({ product }) {
   const { name, price, imageUrl } = product
+  const { addItemToCart } = useContext(CartContext)
+
+  const addProductToCart = () => addItemToCart(product)
 
   return (
     <div className="product-card-container">
@@ -11,7 +16,9 @@ function ProductCard({ product }) {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">Add to cart</Button>
+      <Button onClick={addProductToCart} buttonType="inverted">
+        Add to cart
+      </Button>
     </div>
   )
 }
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,20 +1,49 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 
 export const CartContext = createContext({
   isCartOpen: false,
-  setIsCartOpen: () => null,
-  cartList: [],
-  setCartList: () => null,
+  setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
   cartCount: 0
 })
 
 function CartProvider({ children }) {
   const [isCartOpen, setIsCartOpen] = useState(false)
-  const [cartList, setCartList] = useState([])
+  const [cartItems, setCartItems] = useState([])
+  const [cartCount, setCartCount] = useState(0)
 
-  const cartCount = cartList.length
+  useEffect(() => {
+    const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
+    setCartCount(newCartCount)
+  }, [cartItems])
 
-  const value = { isCartOpen, setIsCartOpen, cartList, setCartList, cartCount }
+  const addItemToCart = item => {
+    if (cartItems.some(cI => cI.id === item.id)) {
+      setCartItems(
+        cartItems.map(cI => {
+          if (cI.id === item.id) {
+            return {
+              ...cI,
+              quantity: cI.quantity + 1
+            }
+          }
+
+          return cI
+        })
+      )
+    } else {
+      setCartItems([
+        ...cartItems,
+        {
+          ...item,
+          quantity: 1
+        }
+      ])
+    }
+  }
+
+  const value = { isCartOpen, setIsCartOpen, cartItems, cartCount, addItemToCart }
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
